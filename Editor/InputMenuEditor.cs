5f9a8a985a169d5f07d64d4137d291d09a560cf0
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2023-06-25 19:15:59 +0200

    fix(core.input): locate input action

--- Editor/InputMenuEditor.cs
+++ Editor/InputMenuEditor.cs
@@ -8,10 +8,13 @@ namespace LemonInc.Core.Input.Editor
 	/// </summary>
 	public class InputMenuEditor
     {
+		/// <summary>
+		/// Locates the input asset.
+		/// </summary>
 		[MenuItem("Tools/LemonInc/Open Input Asset")]
 	    public static void LocateInputAsset()
 	    {
-		    var obj = AssetDatabase.LoadAssetAtPath($"Packages/com.lemon-inc.input/Scripts/Controls.asset", typeof(Object));
+		    var obj = AssetDatabase.LoadAssetAtPath($"Packages/com.lemon-inc.core.input/Scripts/Controls.inputactions", typeof(Object));
 		    Selection.activeObject = obj;
 		    EditorGUIUtility.PingObject(obj);
 	    }
