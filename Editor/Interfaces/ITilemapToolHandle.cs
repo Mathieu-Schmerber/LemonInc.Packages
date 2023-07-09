b1bc3925194e7bdbb9abfbca0901787ac3e4a3b0
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2023-07-09 16:51:12 +0200

    feat(tilemap): tilemap wip

---
+++ Editor/Interfaces/ITilemapToolHandle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LemonInc.Tools.Tilemap.Editor.Interfaces
+{
+	public interface ITilemapToolHandle
+	{
+		public Action<IEnumerable<Vector3Int>> Select { get; set; }
+		public Action<IEnumerable<Vector3Int>> Draw { get; set; }
+		public Action<IEnumerable<Vector3Int>> Erase { get; set; }
+
+		public void OnTileHover(Vector3Int tile);
+		public void OnTileDown(Vector3Int tile);
+		public void OnTileDragged(Vector3Int tile);
+		public void OnTileUp(Vector3Int tile);
+	}
+}
---
+++ Scripts/Data/TileProperties/TileProperty.cs
@@ -0,0 +1,49 @@
+﻿using LemonInc.Core.Utilities;
+using UnityEngine;
+
+namespace LemonInc.Tools.Tilemap.Data.TileProperties
+{
+	/// <summary>
+	/// Define tile properties.
+	/// </summary>
+	[System.Serializable]
+	public class TileProperty 
+	{
+		public enum PatternRotation
+		{
+			Never, _90, _180
+		}
+
+		public enum MirrorOutcome
+		{
+			Never, X, Y, Z, Xy, Xz, Yz, Xyz
+		}
+
+		[System.Serializable]
+		public class NeighborTiling : SerializedDictionary<Vector2Int, TileData> { }
+
+		public NeighborTiling Neighboring;
+		public GameObject Outcome;
+
+		public PatternRotation Rotate;
+		public bool RotateTile;
+		public MirrorOutcome Mirror;
+		public GameObject[] Random;
+
+		public TileProperty(GameObject outcome = null)
+		{
+			Outcome = outcome;
+			Neighboring = new NeighborTiling
+			{
+				{ Vector2Int.up, null },
+				{ Vector2Int.down, null },
+				{ Vector2Int.left, null },
+				{ Vector2Int.right, null },
+				{ Vector2Int.up + Vector2Int.left, null },
+				{ Vector2Int.up + Vector2Int.right, null },
+				{ Vector2Int.down + Vector2Int.left, null },
+				{ Vector2Int.down + Vector2Int.right, null },
+			};
+		}
+	}
+}
