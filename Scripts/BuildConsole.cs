2d6b19951022d78fd56c797646e49e83c4a37bef
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2023-07-09 16:50:19 +0200

    feat(extensions): adding extensions

---
+++ Scripts/BuildConsole.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace LemonInc.Core.Utilities
+{
+	/// <summary>
+	/// https://answers.unity.com/questions/125049/is-there-any-way-to-view-the-console-in-a-build.html
+	/// </summary>
+	public class BuildConsole : MonoBehaviour
+	{
+		private string _logStart = "*** Logs ***";
+		private string _filename = "";
+
+		private const int KChars = 700;
+
+		[SerializeField] private const bool Show = true;
+
+		private void OnEnable() { Application.logMessageReceived += Log; }
+		private void OnDisable() { Application.logMessageReceived -= Log; }
+
+		public void Log(string logString, string stackTrace, LogType type)
+		{
+			_logStart = _logStart + "\n" + logString;
+
+			if (_logStart.Length > KChars)
+				_logStart = _logStart.Substring(_logStart.Length - KChars);
+
+			if (string.IsNullOrEmpty(_filename))
+			{
+				var d = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop) + "/LOGS";
+				System.IO.Directory.CreateDirectory(d);
+				var r = Random.Range(1000, 9999).ToString();
+				_filename = d + "/log-" + r + ".txt";
+			}
+
+			try
+			{
+				System.IO.File.AppendAllText(_filename, logString + "\n");
+			}
+			catch
+			{
+				// ignored
+			}
+		}
+
+		private void OnGUI()
+		{
+			if (!Show) { return; }
+			GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity,
+			   new Vector3(Screen.width / 1200.0f, Screen.height / 800.0f, 1.0f));
+			GUI.TextArea(new Rect(10, 10, 540, 370), _logStart);
+		}
+	}
+}
---
+++ Scripts/ManagerSingleton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace LemonInc.Core.Utilities
+{
+	/// <summary>
+	/// Defines a Singleton. <br/>
+	/// This Singleton will NOT create a new object if it does not exist.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public abstract class ManagerSingleton<T> : MonoBehaviour where T : Component
+	{
+		/// <summary>
+		/// The instance.
+		/// </summary>
+		private static T _instance;
+
+		/// <summary>
+		/// Gets the instance.
+		/// </summary>
+		/// <value>
+		/// The instance.
+		/// </value>
+		public static T Instance
+		{
+			get
+			{
+				if (_instance == null)
+					_instance = GameObject.FindObjectOfType<T>(includeInactive: false);
+				return _instance;
+			}
+		}
+	}
+}
