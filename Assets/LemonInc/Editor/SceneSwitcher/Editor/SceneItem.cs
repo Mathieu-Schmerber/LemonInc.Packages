932a65aae8e34928eaf80ef1ae73d82d40bd525d
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2023-07-14 19:15:58 +0200

    feat(sceneswitcher): ui update

---
+++ Assets/LemonInc/Editor/SceneSwitcher/Editor/SceneItem.cs
@@ -0,0 +1,51 @@
+using System.IO;
+
+namespace LemonInc.Editor.SceneSwitcher
+{
+	/// <summary>
+	/// Describes a scene item.
+	/// </summary>
+	[System.Serializable]
+	public struct SceneItem
+	{
+		/// <summary>
+		/// Gets the name.
+		/// </summary>
+		public string Name => System.IO.Path.GetFileNameWithoutExtension(Path);
+
+		/// <summary>
+		/// Gets the display name.
+		/// </summary>
+		public string DisplayName => $"{Directory.GetParent(Path).Parent.Name}\\{System.IO.Path.GetFileNameWithoutExtension(Path)}";
+
+		/// <summary>
+		/// Gets or sets the path.
+		/// </summary>
+		public string Path { get; set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="SceneItem"/> is active.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if active; otherwise, <c>false</c>.
+		/// </value>
+		public bool Active { get; set; }
+
+		/// <summary>
+		/// Gets or sets the index of the build.
+		/// </summary>
+		public int BuildIndex { get; set; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SceneItem"/> struct.
+		/// </summary>
+		/// <param name="path">The path.</param>
+		/// <param name="buildIndex">Index of the build.</param>
+		public SceneItem(string path, int buildIndex)
+		{
+			Path = path;
+			BuildIndex = buildIndex;
+			Active = false;
+		}
+	}
+}
--- Assets/LemonInc/Editor/SceneSwitcher/Editor/SceneSwitcherWindow.cs
+++ Assets/LemonInc/Editor/SceneSwitcher/Editor/SceneSwitcherWindow.cs
@@ -1,9 +1,9 @@
-using System.IO;
 using System.Linq;
 using LemonInc.Editor.Utilities.Helpers;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace LemonInc.Editor.SceneSwitcher
 {
@@ -13,84 +13,23 @@ namespace LemonInc.Editor.SceneSwitcher
 	/// <seealso cref="UnityEditor.EditorWindow" />
 	public class SceneSwitcherWindow : EditorWindow
 	{
-		#region Types		
-		/// <summary>
-		/// Describes a scene item.
-		/// </summary>
-		[System.Serializable]
-		public struct SceneItem
-		{
-			/// <summary>
-			/// Gets the name.
-			/// </summary>
-			public string Name => System.IO.Path.GetFileNameWithoutExtension(Path);
-
-			/// <summary>
-			/// Gets the display name.
-			/// </summary>
-			public string DisplayName => $"{Directory.GetParent(Path).Parent.Name}\\{System.IO.Path.GetFileNameWithoutExtension(Path)}";
-
-			/// <summary>
-			/// Gets or sets the path.
-			/// </summary>
-			public string Path { get; set; }
-
-			/// <summary>
-			/// Gets or sets the index of the build.
-			/// </summary>
-			public int BuildIndex { get; set; }
-
-			/// <summary>
-			/// Initializes a new instance of the <see cref="SceneItem"/> struct.
-			/// </summary>
-			/// <param name="path">The path.</param>
-			/// <param name="buildIndex">Index of the build.</param>
-			public SceneItem(string path, int buildIndex)
-			{
-				Path = path;
-				BuildIndex = buildIndex;
-			}
-		}
-		#endregion
-
-		/// <summary>
-		/// The styles.
-		/// </summary>
-		public static class Styles
-		{
-			/// <summary>
-			/// The window style.
-			/// </summary>
-			public static GUIStyle WindowStyle = new GUIStyle(GUIStyle.none)
-				.WithNormalBackground(GuiHelper.LightColor)
-				.WithPadding(new RectOffset(1, 1, 1, 1));
-
-			/// <summary>
-			/// Gets the specified base style.
-			/// </summary>
-			/// <param name="baseStyle">The base style.</param>
-			/// <param name="normal">The normal.</param>
-			/// <returns></returns>
-			public static GUIStyle Get(GUIStyle baseStyle, Color normal)
-			{
-				GUIStyle style = new(baseStyle);
-				var ntexture = new Texture2D(1, 1);
-				var htexture = new Texture2D(1, 1);
-
-				ntexture.SetPixel(0, 0, normal);
-				ntexture.Apply();
-				style.normal.background = ntexture;
-
-				htexture.SetPixel(0, 0, new Color(normal.r + .2f, normal.g + .2f, normal.b + .2f));
-				htexture.Apply();
-				style.hover.background = htexture;
-				return style;
-			}
-		}
-
-		private SceneItem[] _scenes;
+		[SerializeField] private VisualTreeAsset _uxml;
+		
+		private static SceneItem[] _scenes;
 		private Vector2 _scollPos;
 		private int _activeScene;
+
+		private ListView _sceneView;
+
+		/// <summary>
+		/// The maximum elements.
+		/// </summary>
+		private const int MAX_ELEMENTS = 10;
+
+		/// <summary>
+		/// The element size.
+		/// </summary>
+		private const int ELEMENT_SIZE = 22;
 
 		/// <summary>
 		/// Opens this instance.
@@ -98,23 +37,50 @@ namespace LemonInc.Editor.SceneSwitcher
 		public static void Open()
 		{
 			var window = ScriptableObject.CreateInstance<SceneSwitcherWindow>();
-			var size = new Vector2(225, 300);
+			_scenes = GetScenes();
+			var size = new Vector2(225, ProcessSize(_scenes.Length));
 			var win = EditorGUIUtility.GetMainWindowPosition();
 
 			// Calculating window ratios to center the window next to the play button
 			var pos = new Vector2(win.x + (win.width * .415f) - (size.x * .5f), win.y + EditorGUIUtility.singleLineHeight * 1.5f);
-
-			window.Init();
-			window.position = new Rect(pos.x, pos.y, size.x, window.GetMinimumHeight());
+			window.position = new Rect(pos.x, pos.y, size.x, size.y );
+			
 			window.ShowPopup();
 			window.Focus();
 		}
 
 		/// <summary>
-		/// Gets the minimum height.
+		/// Processes the size.
+		/// </summary>
+		private static float ProcessSize(int elementNumber) => Mathf.Clamp(elementNumber, 0, MAX_ELEMENTS) * ELEMENT_SIZE + 6; //+2 for border size
+
+		/// <summary>
+		/// Gets the scenes.
 		/// </summary>
 		/// <returns></returns>
-		private float GetMinimumHeight() => (_scenes.Length + 1) * EditorGUIUtility.singleLineHeight + 8;
+		private static SceneItem[] GetScenes()
+		{
+			var loaded = EditorSceneManager.GetAllScenes();
+			var result = SceneHelper.GetAllScenesInProject()
+				.Select(x => new SceneItem(x.Path, x.BuildIndex)
+				{
+					Active = loaded.Any(y => y.name.Equals(x.Name))
+				})
+				.OrderBy(x => x.DisplayName).ToArray();
+
+			return result;
+		}
+
+		/// <summary>
+		/// Creates the GUI.
+		/// </summary>
+		private void CreateGUI()
+		{
+			_uxml.CloneTree(rootVisualElement);
+			_sceneView = rootVisualElement.Q<ListView>();
+
+			Init();
+		}
 
 		/// <summary>
 		/// Gets the index of the active scene.
@@ -136,56 +102,84 @@ namespace LemonInc.Editor.SceneSwitcher
 		/// </summary>
 		private void Init()
 		{
-			_scenes = SceneHelper.GetAllScenesInProject().Select(x => new SceneItem(x.Path, x.BuildIndex)).OrderBy(x => x.DisplayName).ToArray();
 			_activeScene = GetActiveSceneIndex(_scenes);
+
+			_sceneView.makeItem = () =>
+			{
+				var root = new VisualElement()
+				{
+					style =
+					{
+						flexDirection = new StyleEnum<FlexDirection>(FlexDirection.Row),
+						alignItems = new StyleEnum<Align>(Align.Center),
+						justifyContent = new StyleEnum<Justify>(Justify.SpaceBetween)
+					}
+				};
+
+				var label = new Label();
+				var button = new Button()
+				{
+					text = "+"
+				};
+
+				root.Add(label);
+				root.Add(button);
+				return root;
+			};
+
+			_sceneView.bindItem = (element, index) =>
+			{
+				var root = element;
+				var label = root.Q<Label>();
+				var button = root.Q<Button>();
+
+				label.text = _scenes[index].DisplayName;
+				label.style.color = _scenes[index].Active ? new StyleColor(Color.cyan) : new StyleColor(Color.white);
+				button.SetEnabled(!_scenes[index].Active);
+
+				button.clicked += () =>
+				{
+					_scenes[index].Active = true;
+					label.style.color = new StyleColor(Color.cyan);
+					AddScene(_scenes[index]);
+					button.SetEnabled(!_scenes[index].Active);
+				};
+			};
+
+			_sceneView.itemsSource = _scenes;
+			_sceneView.selectionChanged += OnSceneSelected;
+		}
+
+		/// <summary>
+		/// Called when [scene selected].
+		/// </summary>
+		/// <param name="obj">The object.</param>
+		private void OnSceneSelected(System.Collections.Generic.IEnumerable<object> obj)
+		{
+			SceneItem scene = _scenes[_sceneView.selectedIndex];
+
+			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+			EditorSceneManager.OpenScene(scene.Path, OpenSceneMode.Single);
+			Close();
+		}
+
+		/// <summary>
+		/// Adds the scene.
+		/// </summary>
+		/// <param name="scene">The scene.</param>
+		private void AddScene(SceneItem scene)
+		{
+			EditorSceneManager.OpenScene(scene.Path, OpenSceneMode.Additive);
 		}
 
 		/// <summary>
 		/// Closes the window.
 		/// </summary>
-		private void CloseWindow()
-		{
-			Close();
-		}
+		private void CloseWindow() => Close();
 
 		/// <summary>
 		/// Called when [lost focus].
 		/// </summary>
 		private void OnLostFocus() => CloseWindow();
-
-		/// <summary>
-		/// Called when [GUI].
-		/// </summary>
-		private void OnGUI()
-		{
-			var evenBtn = EditorStyles.toolbarButton;
-			var oddBtn = Styles.Get(EditorStyles.toolbarButton, new Color(.2f, .2f, .2f, 1f));
-
-			EditorGUILayout.BeginVertical(Styles.WindowStyle);
-			{
-				_scollPos = EditorGUILayout.BeginScrollView(_scollPos);
-				{
-					for (var i = 0; i < _scenes.Length; i++)
-					{
-						var item = _scenes[i];
-
-						EditorGUILayout.BeginHorizontal(i % 2 == 0 ? evenBtn : oddBtn);
-						{
-							if (GUILayout.Button(item.DisplayName, i == _activeScene ? EditorStyles.boldLabel : EditorStyles.label))
-							{
-								EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
-								EditorSceneManager.OpenScene(item.Path, OpenSceneMode.Single);
-								Close();
-							}
-							if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.ExpandHeight(true)))
-								EditorSceneManager.OpenScene(item.Path, OpenSceneMode.Additive);
-						}
-						EditorGUILayout.EndHorizontal();
-					}
-				}
-				EditorGUILayout.EndScrollView();
-			}
-			EditorGUILayout.EndVertical();
-		}
 	}
 }
