335a5b1c0d0fa978091b7db51055fc696624e83a
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2023-08-12 19:50:48 +0200

    fix(utilities)

--- Scripts/Extensions/EnumerableExtension.cs
+++ Scripts/Extensions/EnumerableExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -44,6 +45,32 @@ namespace LemonInc.Core.Utilities.Extensions
 		{
 			foreach (var item in source)
 				action(item);
+		}
+
+		/// <summary>
+		/// Determines whether [is null or empty].
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="source">The source.</param>
+		/// <returns>
+		///   <c>true</c> if [is null or empty] [the specified source]; otherwise, <c>false</c>.
+		/// </returns>
+		public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
+		{
+			return source == null || !source.Any();
+		}
+
+		/// <summary>
+		/// Determines whether [is null or empty].
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="source">The source.</param>
+		/// <returns>
+		///   <c>true</c> if [is null or empty] [the specified source]; otherwise, <c>false</c>.
+		/// </returns>
+		public static bool IsNullOrEmpty(this IList source)
+		{
+			return source == null || source.Count == 0;
 		}
 	}
 }
