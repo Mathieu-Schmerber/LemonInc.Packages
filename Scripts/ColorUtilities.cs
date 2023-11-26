0fcededc02002ab5417d5ac145e0131629b18fbb
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2023-11-26 13:57:23 +0100

    feat(core.utilities): lemonIncEditorUtilities draw framework

---
+++ Scripts/ColorUtilities.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace LemonInc.Core.Utilities
+{
+	/// <summary>
+	/// Color utilities.
+	/// </summary>
+	public static class ColorUtilities
+	{
+		/// <summary>
+		/// Randoms a color.
+		/// </summary>
+		/// <param name="alpha">The alpha.</param>
+		/// <returns>The <see cref="Color32"/>.</returns>
+		public static Color32 RandomColor(int alpha = 255)
+		{
+			System.Random random = new();
+
+			return new Color32(
+				(byte)random.Next(0, 255),
+				(byte)random.Next(0, 255),
+				(byte)random.Next(0, 255),
+				(byte)alpha
+			);
+		}
+
+		/// <summary>
+		/// Randoms a color.
+		/// </summary>
+		/// <param name="seed">The seed.</param>
+		/// <param name="alpha">The alpha.</param>
+		/// <returns>The <see cref="Color32"/>.</returns>
+		public static Color32 RandomColor(string seed, int alpha = 255)
+		{
+			System.Random random = new(seed.GetHashCode());
+
+			return new Color32(
+				(byte)random.Next(0, 255),
+				(byte)random.Next(0, 255),
+				(byte)random.Next(0, 255),
+				(byte)alpha
+			);
+		}
+	}
+}
--- Scripts/Extensions/ColorExtensions.cs
+++ Scripts/Extensions/ColorExtensions.cs
@@ -14,40 +14,5 @@ namespace LemonInc.Core.Utilities.Extensions
 		/// <param name="alpha">The alpha.</param>
 		/// <returns>The <see cref="Color"/>.</returns>
 		public static Color WithAlpha(this Color color, float alpha) => new(color.r, color.g, color.b, alpha);
-
-		/// <summary>
-		/// Randoms a color.
-		/// </summary>
-		/// <param name="alpha">The alpha.</param>
-		/// <returns>The <see cref="Color32"/>.</returns>
-		public static Color32 RandomColor(int alpha = 255)
-		{
-			System.Random random = new();
-
-			return new Color32(
-				(byte)random.Next(0, 255),
-				(byte)random.Next(0, 255),
-				(byte)random.Next(0, 255),
-				(byte)alpha
-			);
-		}
-
-		/// <summary>
-		/// Randoms a color.
-		/// </summary>
-		/// <param name="seed">The seed.</param>
-		/// <param name="alpha">The alpha.</param>
-		/// <returns>The <see cref="Color32"/>.</returns>
-		public static Color32 RandomColor(string seed, int alpha = 255)
-		{
-			System.Random random = new(seed.GetHashCode());
-
-			return new Color32(
-				(byte)random.Next(0, 255),
-				(byte)random.Next(0, 255),
-				(byte)random.Next(0, 255),
-				(byte)alpha
-			);
-		}
 	}
 }
