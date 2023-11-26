4522360ca973e4aaaf1594d2fe73556155843b02
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2023-11-26 15:42:53 +0100

    update

--- Assets/Plugins/LemonInc/Resources/Panels/Panels.cs
+++ Assets/Plugins/LemonInc/Resources/Panels/Panels.cs
@@ -1,16 +1,19 @@
 
+				using System.Collections.Generic;
+				using LemonInc.Tools.Panels.Models;
 				using UnityEditor;
 				using UnityEngine;
 				namespace LemonInc.Generated {
 			        internal static class PanelMenuItems
 			        {
 			            
-		        [MenuItem("Tools/LemonInc/Panels/TestPanel")]
-		        public static void OpenTestPanel()
+		        [MenuItem("Tools/LemonInc/Panels/GD")]
+		        public static void OpenGd()
 		        {
 		            var window = UnityEditor.EditorWindow.CreateWindow<LemonInc.Tools.Panels.PanelEditorWindow>();
-		            window.titleContent = new GUIContent("TestPanel");
-					window.Init("TestPanel");
+		            
+					window.titleContent = new GUIContent("GD");
+					window.Init("GD");
 		            window.Show();
 		            window.Focus();
 		        }
