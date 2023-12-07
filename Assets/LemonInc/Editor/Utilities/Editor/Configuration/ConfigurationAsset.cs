e32555b7eab1e466e98b98a99aacd76583abd8cb
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2023-12-07 11:19:30 +0100

    feat(editor.utilities): configuration utility

---
+++ Assets/LemonInc/Editor/Utilities/Editor/Configuration/ConfigurationAsset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace LemonInc.Editor.Utilities.Configuration
+{
+	/// <summary>
+	/// Describes a configuration asset.
+	/// </summary>
+	/// <seealso cref="UnityEngine.ScriptableObject" />
+	/// <seealso cref="LemonInc.Editor.Utilities.Configuration.IConfiguration" />
+	public class ConfigurationAsset : ScriptableObject, IConfiguration
+	{
+		/// <inheritdoc/>
+		public virtual void OnCreated() { }
+
+		/// <inheritdoc/>
+		public virtual void OnLoaded() { }
+	}
+}
---
+++ Assets/LemonInc/Editor/Utilities/Editor/Configuration/ConfigurationLoader.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace LemonInc.Editor.Utilities.Configuration
+{
+	/// <summary>
+	/// Loads configuration.
+	/// </summary>
+	public static class ConfigurationLoader
+	{
+		/// <summary>
+		/// Gets the configuration.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="path">The path.</param>
+		/// <returns>The configuration or null if it does not exist.</returns>
+		private static T GetConfiguration<T>(string path)
+			where T : Object, IConfiguration
+			=> Application.isPlaying ? Resources.Load<T>(path) : AssetDatabase.LoadAssetAtPath<T>(path);
+
+		/// <summary>
+		/// Creates the configuration.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="path">The path.</param>
+		/// <returns>The created configuration.</returns>
+		private static T CreateConfiguration<T>(string path)
+			where T : ConfigurationAsset
+		{
+			var created = ScriptableObject.CreateInstance<T>();
+			var directory = Path.GetDirectoryName(path);
+			Directory.CreateDirectory(directory);
+
+			AssetDatabase.CreateAsset(created, path);
+			AssetDatabase.SaveAssets();
+			AssetDatabase.Refresh();
+			return created;
+		}
+
+		/// <summary>
+		/// Loads the configuration.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="assetPath">The path.</param>
+		/// <returns>The configuration or null.</returns>
+		public static T LoadConfiguration<T>(string assetPath)
+			where T : ConfigurationAsset
+		{
+			var path = Path.Combine("Assets", assetPath);
+			var result = GetConfiguration<T>(path);
+			if (result == null)
+			{
+				result = CreateConfiguration<T>(path);
+				result.OnCreated();
+			}
+
+			result.OnLoaded();
+			return result;
+		}
+	}
+}
