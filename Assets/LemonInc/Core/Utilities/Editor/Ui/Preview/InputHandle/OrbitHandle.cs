a7cb0b69863aa27129a511dce55f31efefb568bf
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2025-03-04 21:22:29 +0100

    feat(core.utilities): editor preview

--- Assets/LemonInc/Core/Utilities/Editor/Ui/Preview/InputHandle/OrbitHandle.cs
+++ Assets/LemonInc/Core/Utilities/Editor/Ui/Preview/InputHandle/OrbitHandle.cs
@@ -1,16 +1,52 @@
+using System.Runtime.Remoting.Messaging;
 using UnityEngine;
 
 namespace LemonInc.Core.Utilities.Editor.Ui.Preview.InputHandle
 {
-    public class OrbitHandle : IInputHandleStrategy
+    /// <summary>
+    /// Orbits around a center point by dragging while pressing right click.
+    /// </summary>
+    public class OrbitHandle : IEventHandleStrategy
     {
-        public bool Active { get; set; }
-     
-        public void OnInput(IEditorPreview preview, Event current)
-        {
-            
-        }
+        private bool _isRightMouseButtonDown;
         
+        public Vector3 OrbitCenter { get; set; }
+        public bool Active { get; set; }
+    
+        public bool ConsumeEvent(IEditorPreview preview, Event current)
+        {
+            if (!Active || preview?.Camera == null)
+                return false;
+
+            switch (current.type)
+            {
+                case EventType.MouseDown when current.button == 1:
+                    _isRightMouseButtonDown = true;
+                    break;
+                case EventType.MouseUp when current.button == 1:
+                    _isRightMouseButtonDown = false;
+                    break;
+                case EventType.MouseDrag when _isRightMouseButtonDown:
+                    if (current.delta.sqrMagnitude > 0)
+                        RotateCamera(preview.Camera.transform, current.delta);
+                    break; 
+                default:
+                    return false;
+            }
+
+            return true;
+        }
+
+        private void RotateCamera(Transform cameraTransform, Vector2 delta)
+        {
+            var mouseX = delta.x;
+            var mouseY = delta.y;
+
+            cameraTransform.RotateAround(OrbitCenter, Vector3.up, mouseX);
+            cameraTransform.RotateAround(OrbitCenter, cameraTransform.right, mouseY);
+            cameraTransform.LookAt(OrbitCenter);
+        }
+
         public void Dispose() { }
     }
 }
