413a8742c7b5dbba5d26b37ef3b6eb6d18e20db2
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2025-03-04 21:21:25 +0100

    wip(statemachine)

---
+++ Assets/LemonInc/Core/Utilities/Editor/Ui/Preview/InputHandle/IScrollHandleStrategy.cs
@@ -0,0 +1,7 @@
+namespace LemonInc.Core.Utilities.Editor.Ui.Preview.InputHandle
+{
+    public interface IScrollHandleStrategy
+    {
+        
+    }
+}
---
+++ Assets/LemonInc/Test/TestSoEditor.cs
@@ -0,0 +1,7 @@
+namespace LemonInc.Test
+{
+    public class TestSoEditor
+    {
+        
+    }
+}
