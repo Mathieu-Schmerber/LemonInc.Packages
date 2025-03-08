cd6165e46ece07dc0a7409843accf3671a7330d8
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2025-03-08 20:58:27 +0100

    refactor(core.utilities): timer refactores and added more unity extensions

--- Assets/LemonInc/Core/Utilities/Editor/TimerPropertyDrawer.cs
+++ Assets/LemonInc/Core/Utilities/Editor/TimerPropertyDrawer.cs
@@ -1,7 +1,42 @@
+using UnityEditor;
+using UnityEngine;
+
 namespace LemonInc.Core.Utilities.Editor
 {
-    public class TimerPropertyDrawer
+    [CustomPropertyDrawer(typeof(Timer))]
+    public class TimerPropertyDrawer : PropertyDrawer
     {
-        
+        private const float LINE_HEIGHT = 18f;
+        private const float INTERVAL_FIELD_WIDTH = 50f;
+        private const float PROGRESS_BAR_PADDING = 5f;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            var labelRect = new Rect(position.x, position.y, EditorGUIUtility.labelWidth, LINE_HEIGHT);
+            var intervalRect = new Rect(position.x + EditorGUIUtility.labelWidth, position.y, INTERVAL_FIELD_WIDTH, LINE_HEIGHT);
+            var progressBarRect = new Rect(
+                intervalRect.x + intervalRect.width + PROGRESS_BAR_PADDING,
+                position.y,
+                position.width - EditorGUIUtility.labelWidth - INTERVAL_FIELD_WIDTH - PROGRESS_BAR_PADDING,
+                LINE_HEIGHT);
+
+            EditorGUI.LabelField(labelRect, label);
+            
+            var intervalProp = property.FindPropertyRelative("_interval");
+            EditorGUI.PropertyField(intervalRect, intervalProp, GUIContent.none);
+
+            var elapsedTimeProp = property.FindPropertyRelative("_elapsedTime");
+            var elapsedTime = elapsedTimeProp.floatValue;
+            var intervalValue = intervalProp.floatValue;
+            var progress = intervalValue > 0 ? Mathf.Clamp01(elapsedTime / intervalValue) : 0;
+
+            EditorGUI.ProgressBar(progressBarRect, progress, $"{elapsedTime:0.00} / {intervalValue:0.00}s");
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+            => LINE_HEIGHT;
     }
 }
--- Assets/LemonInc/Core/Utilities/Scripts/Extensions/ColorExtensions.cs
+++ Assets/LemonInc/Core/Utilities/Scripts/Extensions/ColorExtensions.cs
@@ -14,5 +14,48 @@ namespace LemonInc.Core.Utilities.Extensions
 		/// <param name="alpha">The alpha.</param>
 		/// <returns>The <see cref="Color"/>.</returns>
 		public static Color WithAlpha(this Color color, float alpha) => new(color.r, color.g, color.b, alpha);
+
+		/// <summary>
+		/// Gets the color with the provided red component.
+		/// </summary>
+		/// <param name="color">The color.</param>
+		/// <param name="red">The red value.</param>
+		/// <returns>The <see cref="Color"/>.</returns>
+		public static Color WithRed(this Color color, float red) => new(red, color.g, color.b, color.a);
+
+		/// <summary>
+		/// Gets the color with the provided green component.
+		/// </summary>
+		/// <param name="color">The color.</param>
+		/// <param name="green">The green value.</param>
+		/// <returns>The <see cref="Color"/>.</returns>
+		public static Color WithGreen(this Color color, float green) => new(color.r, green, color.b, color.a);
+
+		/// <summary>
+		/// Gets the color with the provided blue component.
+		/// </summary>
+		/// <param name="color">The color.</param>
+		/// <param name="blue">The blue value.</param>
+		/// <returns>The <see cref="Color"/>.</returns>
+		public static Color WithBlue(this Color color, float blue) => new(color.r, color.g, blue, color.a);
+
+		/// <summary>
+		/// Gets the color with optional modifications to its components.
+		/// </summary>
+		/// <param name="color">The color.</param>
+		/// <param name="r">The optional red value.</param>
+		/// <param name="g">The optional green value.</param>
+		/// <param name="b">The optional blue value.</param>
+		/// <param name="a">The optional alpha value.</param>
+		/// <returns>The <see cref="Color"/>.</returns>
+		public static Color With(this Color color, float? r = null, float? g = null, float? b = null, float? a = null)
+		{
+			return new Color(
+				r ?? color.r,
+				g ?? color.g,
+				b ?? color.b,
+				a ?? color.a
+			);
+		}
 	}
 }
