9a6282e9bb1fee8bc5b7afccf4fc05e2664012c3
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2025-03-11 14:53:04 +0100

    feat(core.utilities): editor scriptable extensions, child asset management

--- Assets/LemonInc/Core/Utilities/Editor/Extensions/ObjectExtensions.cs
+++ Assets/LemonInc/Core/Utilities/Editor/Extensions/ObjectExtensions.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 namespace LemonInc.Core.Utilities.Editor.Extensions
@@ -33,6 +34,53 @@ namespace LemonInc.Core.Utilities.Editor.Extensions
 			where T : Object
 		{
 			return AssetDatabase.GetAssetPath(asset);
+		}
+		
+		/// <summary>
+		/// Creates a child scriptable object of a given parent.
+		/// </summary>
+		/// <param name="parent">The parent asset.</param>
+		/// <typeparam name="T"></typeparam>
+		/// <returns>The created scriptable.</returns>
+		public static T CreateChildScriptable<T>(this Object parent) where T : ScriptableObject
+		{
+			var child = ScriptableObject.CreateInstance<T>();
+			AssetDatabase.AddObjectToAsset(child, parent);
+			AssetDatabase.SaveAssets();
+			return child;
+		}
+		
+		/// <summary>
+		/// Retrieves all child assets of the specified type that are associated with the given parent asset.
+		/// </summary>
+		/// <typeparam name="T">The type of ScriptableObject to retrieve.</typeparam>
+		/// <param name="parent">The parent asset.</param>
+		/// <returns>An array of child assets of type T, or null if the parent asset has no path.</returns>
+		public static T[] GetChildAssetsOfType<T>(this Object parent) where T : ScriptableObject
+		{
+			var path = parent.GetPath();
+			if (string.IsNullOrEmpty(path)) 
+				return null;
+
+			return AssetDatabase.LoadAllAssetsAtPath(path)
+				.OfType<T>()
+				.Where(asset => asset != parent)
+				.ToArray();
+		}
+		
+		/// <summary>
+		/// Retrieves the first child asset of the specified type that is associated with the given parent asset.
+		/// </summary>
+		/// <typeparam name="T">The type of ScriptableObject to retrieve.</typeparam>
+		/// <param name="parent">The parent asset.</param>
+		/// <returns>The first child asset of type T, or null if none are found.</returns>
+		public static T GetChildAssetOfType<T>(this Object parent) where T : ScriptableObject
+		{
+			var path = parent.GetPath();
+			if (string.IsNullOrEmpty(path)) 
+				return null;
+
+			return AssetDatabase.LoadAllAssetsAtPath(path).FirstOrDefault(x => x.GetType() == typeof(T)) as T;
 		}
 	}
 }
