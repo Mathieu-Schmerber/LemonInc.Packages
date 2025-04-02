bfe6425696f4c3f11dc6a6e3b2eb675b96028a6a
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2025-04-02 17:26:42 +0200

    feat(core.statemachine): scriptable state machine

---
+++ Scripts/Implementations/Scriptable/ScriptableStateMachine.cs
@@ -0,0 +1,56 @@
+using System;
+using LemonInc.Core.StateMachine.Interfaces;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace LemonInc.Core.StateMachine.Implementations.Scriptable
+{
+    public class ScriptableStateMachine : ScriptableObject, IStateMachine
+    {
+        [SerializeField, InlineProperty] private StateMachine _stateMachine = new();
+        
+        public IState CurrentState => _stateMachine.CurrentState;
+
+        public IState GetState<T>() where T : IState 
+            => _stateMachine.GetState<T>();
+
+        public ISubStateMachine GetSubStateMachine<T>() where T : ISubStateMachine 
+            => _stateMachine.GetSubStateMachine<T>();
+
+        public ISubStateMachine RegisterSubStateMachine(ISubStateMachine subStateMachine) 
+            => _stateMachine.RegisterSubStateMachine(subStateMachine);
+
+        public ISubStateMachine RegisterSubStateMachine<T>() where T : ISubStateMachine, new() 
+            => _stateMachine.RegisterSubStateMachine<T>();
+
+        public IStateMachine RegisterState(IState state)
+        {
+            _stateMachine.RegisterState(state);
+            return this;
+        }
+
+        public IStateMachine RegisterState<T>() where T : IState, new()
+        {
+            _stateMachine.RegisterState<T>();
+            return this;
+        }
+
+        public void AddLink<TFrom, TTo>(Func<bool> condition) where TFrom : IState where TTo : IState 
+            => _stateMachine.AddLink<TFrom, TTo>(condition);
+
+        public void AddMutualLink<TFrom, TTo>(Func<bool> condition) where TFrom : IState where TTo : IState 
+            => _stateMachine.AddMutualLink<TFrom, TTo>(condition);
+
+        public void SetActiveState(IState state) 
+            => _stateMachine.SetActiveState(state);
+
+        public void SetActiveState<T>() where T : IState 
+            => _stateMachine.SetActiveState<T>();
+
+        public void Update() 
+            => _stateMachine.Update();
+
+        public void FixedUpdate() 
+            => _stateMachine.FixedUpdate();
+    }
+}
