7c31deeccb07efe9938f10a48ab2d1fd4683519c
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2025-06-27 15:13:43 +0200

    chore(core.utilities): cleaner console

--- Editor/SetupUtils.cs
+++ Editor/SetupUtils.cs
@@ -6,7 +6,6 @@ using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
-using UnityEngine.WSA;
 using static System.Environment;
 using static System.IO.Path;
 using static UnityEditor.AssetDatabase;
