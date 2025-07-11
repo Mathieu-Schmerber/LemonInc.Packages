f39032e69d8f70ae191f95a1de7808ffe69d9576
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2025-07-11 11:54:30 +0200

    chore(core.input): TouchInput renamed to GestureInput

--- Assets/LemonInc/Core/Input/Scripts/GestureInput.cs
+++ Assets/LemonInc/Core/Input/Scripts/GestureInput.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 namespace LemonInc.Core.Input
 {
-    public class TouchInput
+    public class GestureInput
     {
         private readonly float _maxSwipeTime;
         private readonly float _minSwipeDistance;
@@ -75,7 +75,7 @@ namespace LemonInc.Core.Input
             Touched.OnReleased -= OnTouchEnd;
         }
 
-        public TouchInput(float maxSwipeTime = 1f, float minSwipeDistance = 100f)
+        public GestureInput(float maxSwipeTime = 1f, float minSwipeDistance = 100f)
         {
             _maxSwipeTime = maxSwipeTime;
             _minSwipeDistance = minSwipeDistance;
