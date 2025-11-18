8e640c60f11fb42525962cc75394a397c2b9c0df
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2025-11-18 23:42:06 +0100

    feat(core.utilities): scene attribute

---
+++ Assets/LemonInc/Core/Utilities/Scripts/Attributes/SceneAttribute.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace LemonInc.Core.Utilities.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
+    public class SceneAttribute : Attribute { }
+}
